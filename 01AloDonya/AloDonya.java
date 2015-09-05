4dad4b298b729f264f30a56dc5e1121fd681eb37
sayedhalik <sayedhalik@gmail.com>
2015-09-05 17:18:12 +0700

    Project_1

---
+++ 01AloDonya/AloDonya.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class AloDonya here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.io.IOException;
+public class AloDonya
+{
+    
+    public static void main(String[] args) throws IOException{
+        System.out.print("Hallo Donya");
+    }
+    
+}
